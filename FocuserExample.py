4a9ed915e5e85789ab99332adc743ee8a95e3cb4
Alex <aser@jooble.com>
2023-09-02 17:09:40 +0300

    start

--- FocuserExample.py
+++ FocuserExample.py
@@ -17,38 +17,47 @@ def RenderStatusBar(stdscr):
 
 def RenderMiddleText(stdscr, k, focuser):
     height, width = stdscr.getmaxyx()
-    subtitle = ""[:width - 1]
-    keystr = "Last key pressed: {}".format(k)[:width - 1]
+    # subtitle = ""[:width - 1]
+    # keystr = "Last key pressed: {}".format(k)[:width - 1]
 
-    focus_value = "Focus (Left-Right Arrow) : {}".format(focuser.get(Focuser.OPT_FOCUS))[:width - 1]
-    zoom_value = "Zoom (Up-Down Arrow)      : {}".format(focuser.get(Focuser.OPT_ZOOM))[:width - 1]
-    motor_x_val = "MotorX ('w'-'s' Key)     : {}".format(focuser.get(Focuser.OPT_MOTOR_X))[:width - 1]
-    motor_y_val = "MotorY ('a'-'d' Key)     : {}".format(focuser.get(Focuser.OPT_MOTOR_Y))[:width - 1]
-    ircut_val = "IRCUT    : {}".format(focuser.get(Focuser.OPT_IRCUT))[:width - 1]
+    combined_values = "Last key pressed: {} Focus (Left-Right Arrow): {} Zoom (Up-Down Arrow): {} MotorX ('w'-'s' Key): {} MotorY ('a'-'d' Key): {} IRCUT: {}".format(
+        k,
+        focuser.get(Focuser.OPT_FOCUS)[:width - 1],
+        focuser.get(Focuser.OPT_ZOOM)[:width - 1],
+        focuser.get(Focuser.OPT_MOTOR_X)[:width - 1],
+        focuser.get(Focuser.OPT_MOTOR_Y)[:width - 1],
+        focuser.get(Focuser.OPT_IRCUT)[:width - 1]
+    )
 
-    if k == 0:
-        keystr = "No key press detected..."[:width - 1]
+    # focus_value = "Focus (Left-Right Arrow) : {}".format(focuser.get(Focuser.OPT_FOCUS))[:width - 1]
+    # zoom_value = "Zoom (Up-Down Arrow)      : {}".format(focuser.get(Focuser.OPT_ZOOM))[:width - 1]
+    # motor_x_val = "MotorX ('w'-'s' Key)     : {}".format(focuser.get(Focuser.OPT_MOTOR_X))[:width - 1]
+    # motor_y_val = "MotorY ('a'-'d' Key)     : {}".format(focuser.get(Focuser.OPT_MOTOR_Y))[:width - 1]
+    # ircut_val = "IRCUT    : {}".format(focuser.get(Focuser.OPT_IRCUT))[:width - 1]
 
-    start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
-    start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
-    start_x_device_info = int((width // 2) - (len("Focus    : 00000") // 2) - len("Focus    : 00000") % 2)
-    start_y = int((height // 2) - 6)
+    # if k == 0:
+    #     keystr = "No key press detected..."[:width - 1]
 
-    stdscr.attron(curses.color_pair(2))
-    stdscr.attron(curses.A_BOLD)
+    # start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
+    # start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
+    # # start_x_device_info = int((width // 2) - (len("Focus    : 00000") // 2) - len("Focus    : 00000") % 2)
+    # start_y = int((height // 2) - 6)
+    #
+    # stdscr.attron(curses.color_pair(2))
+    # stdscr.attron(curses.A_BOLD)
+    #
+    # stdscr.attroff(curses.color_pair(2))
+    # stdscr.attroff(curses.A_BOLD)
+    #
+    # stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
+    # stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
+    stdscr.addstr(0, 0, combined_values)
 
-    stdscr.attroff(curses.color_pair(2))
-    stdscr.attroff(curses.A_BOLD)
-
-    stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
-    stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
-    stdscr.addstr(start_y + 5, start_x_keystr, keystr)
-
-    stdscr.addstr(start_y + 6, start_x_device_info, focus_value)
-    stdscr.addstr(start_y + 7, start_x_device_info, zoom_value)
-    stdscr.addstr(start_y + 8, start_x_device_info, motor_x_val)
-    stdscr.addstr(start_y + 9, start_x_device_info, motor_y_val)
-    stdscr.addstr(start_y + 10, start_x_device_info, ircut_val)
+    # stdscr.addstr(start_y + 6, start_x_device_info, focus_value)
+    # stdscr.addstr(start_y + 7, start_x_device_info, zoom_value)
+    # stdscr.addstr(start_y + 8, start_x_device_info, motor_x_val)
+    # stdscr.addstr(start_y + 9, start_x_device_info, motor_y_val)
+    # stdscr.addstr(start_y + 10, start_x_device_info, ircut_val)
 
 
 def parseKey(k, focuser):
@@ -65,6 +74,7 @@ def parseKey(k, focuser):
         focuser.set(Focuser.OPT_MOTOR_X, focuser.get(Focuser.OPT_MOTOR_X) - motor_step)
     elif k == ord('a'):
         focuser.set(Focuser.OPT_MOTOR_X, focuser.get(Focuser.OPT_MOTOR_X) + motor_step)
+
     if k == ord('r'):
         focuser.reset(Focuser.OPT_FOCUS)
         focuser.reset(Focuser.OPT_ZOOM)
@@ -94,12 +104,8 @@ def draw_menu(stdscr, i2c_bus):
     while (k != ord('q')):
         stdscr.clear()
         curses.flushinp()
-        height, width = stdscr.getmaxyx()
 
         parseKey(k, focuser)
-
-        # whstr = "Width: {}, Height: {}".format(width, height)
-        # stdscr.addstr(0, 0, whstr, curses.color_pair(1))
 
         RenderStatusBar(stdscr)
         RenderMiddleText(stdscr, k, focuser)
