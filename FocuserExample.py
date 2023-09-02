84d9ebc62991cccaf2a5eb0ca0fa1e642cfb02ee
Alex <aser@jooble.com>
2023-09-02 17:43:28 +0300

    start

--- FocuserExample.py
+++ FocuserExample.py
@@ -69,10 +69,10 @@ def draw_menu(stdscr, i2c_bus):
     stdscr.clear()
     stdscr.refresh()
 
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    # curses.start_color()
+    # curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    # curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    # curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
     while (k != ord('q')):
         stdscr.clear()
